0aa0804e034e8ec6352a0ed9a871d69bd6e89967
Seung Hyun Kim <skim0119@gmail.com>
2019-06-16 17:33:27 -0500

    update

--- utility/RL_Wrapper.py
+++ utility/RL_Wrapper.py
@@ -5,97 +5,101 @@ import numpy as np
 import tensorflow as tf
 
 from utility.utils import store_args
+
 from utility.utils import discount_rewards
 
 
-class Subgraph:
-    """Subgraph
-
-    Import pre-trained model for simulation and training.
-    
-    It does not initialize model.
+class Model:
     """
+    Model wraps the existing method and provide common methods.
+        - train
+        - save
+        - load
+        - run
+    """
+
     @store_args
     def __init__(
         self,
-        in_size,
-        model_name,
-        network_type=None,
+        model_path=None,
+        log_path=None,
+        name=None,
         sess=None,
-        global_network=None,
-        record=False,
+        prohibit_log=False,
         *args,
         **kwargs
     ):
-        if sess is None:
-            self.graph = tf.Graph()
-            self.sess = tf.Session(graph=self.graph)
+        """ __init__
+        Parameters
+        ----------------
+
+        prohibit_log : [bool] 
+            If true, any log in this model will be ignored.
+            It mainly prohibit logging in train
+        """
+        # Assertions
+        assert model_path is not None, 'model path is not specified'
+        assert log_path is not None, 'model path is not specified'
+        assert sess is not None and name is None, 'If pre-defiend session is used, must provide name of this network'
+        assert name == 'global', 'Model name cannot be global'
+       
+        # Presets
+        if name is not None:
+            input_tensor = name + '/'  + input_tensor
+            output_tensor = name + '/'  + output_tensor
+
+        # Find checkpoint
+        ckpt = tf.train.get_checkpoint_state(model_path)
+        if not ckpt or not tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+            raise NameError('Error : Graph failed to load')
         else:
-            self.graph = self.sess.graph
+            print('Checkpoint Found.')
+            print('Call TF pretrained model:')
+            print('    checkpoint_path : {}'.format(ckpt.model_checkpoint_path))
+            print('    input_name : {}'.format(input_name))
+            print('    output_name : {}'.format(output_name))
 
-        with self.sess.as_default(), self.graph.as_default():
-            self.network = network_type(
-                in_size=in_size,
-                action_size=5,
-                lr_actor=5e-5,
-                lr_critic=2e-4,
-                scope=model_name,
-                entropy_beta = 0.01,
-                sess=sess,
-                global_network=global_network
-            )
-            self.network.initialize_vars()
-            self.network.pull_global()
+        # Set TF graph and session
+        if sess is None:
+            config = tf.ConfigProto(device_count = {'GPU': 0})  # Only use CPU
+            self.graph = tf.Graph()
+            self.sess = tf.Session(config=config, graph=self.graph)
+        else:  # Use given graph
+            self.graph = sess.graph
 
+        # Load and restore TF model
+        with self.graph.as_default():
+            self.saver = tf.train.import_meta_graph(
+                    meta_graph_or_file=ckpt.model_checkpoint_path+'.meta',
+                    clear_devices=True,
+                    import_scope=name,
+                )
+            iuv(self.sess)  # Initialize all undefined weights
+        self.state, self.action = self.reset_network_weight()
+        print('    TF policy loaded. {}'.format(name) )
 
-    def get_action(self, states):
-        actions, values = self.network.run_network(states)
-        return actions, values
+    def run_network(self, input_tensor):
+        with self.sess.as_default(), self.sess.graph.as_default():
+            feed_dict = {self.state: input_tensor}
+            action_prob = self.sess.run(self.action, feed_dict)
 
-    def train(self, trajs, bootstrap=0.0, gamma=0.98):
-        buffer_s, buffer_a, buffer_tdtarget, buffer_adv = [], [], [], []
-        for idx, traj in enumerate(trajs):
-            if len(traj) == 0:
-                continue
-            observations = traj[0]
-            actions = traj[1]
-            rewards = np.array(traj[2])
-            values = np.array(traj[3])
-            
-            value_ext = np.append(values, [bootstrap[idx]])
-            td_target  = rewards + gamma * value_ext[1:]
-            advantages = rewards + gamma * value_ext[1:] - value_ext[:-1]
-            advantages = discount_rewards(advantages,gamma)
-            
-            buffer_s.extend(observations)
-            buffer_a.extend(actions)
-            buffer_tdtarget.extend(td_target.tolist())
-            buffer_adv.extend(advantages.tolist())
+        return action_prob
 
-        if len(buffer_s) == 0:
-            return
-
-        # Stack buffer
-        feed_dict = {
-            self.network.state_input : np.stack(buffer_s),
-            self.network.action_ : np.array(buffer_a),
-            self.network.td_target_ : np.array(buffer_tdtarget),
-            self.network.advantage_ : np.array(buffer_adv),
-        }
-
-        # Update Buffer
-        self.network.update_global(
-            buffer_s,
-            buffer_a,
-            buffer_tdtarget,
-            buffer_adv
-        )
-
-        # get global parameters to local ActorCritic 
-        self.network.pull_global()
-        
-        return 
-    
+    def reset_network_weight(self):
+        """
+        Reload the weight from the TF meta data
+        """
+        input_name = self.input_name
+        output_name = self.output_name
+        with self.sess.graph.as_default():
+            ckpt = tf.train.get_checkpoint_state(self.model_dir)
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+            state = self.graph.get_tensor_by_name(input_name)
+            try:
+                action = self.graph.get_operation_by_name(output_name)
+            except ValueError:
+                action = self.graph.get_tensor_by_name(output_name)
+        return state, action
 
 
 class TrainedNetwork:
@@ -103,77 +107,82 @@ class TrainedNetwork:
 
     Import pre-trained model for simulation only.
     
-    It does not include any training sequence.
+    - It does not include any training sequence.
+    - It provides method to reload network weights.
+    - It does not alter the network.
     """
 
     @store_args
     def __init__(
         self,
-        model_name,
+        model_path=None,
         input_tensor='global/state:0',
         output_tensor='global/actor/fully_connected_1/Softmax:0',
-        action_space=5,
+        name=None,
+        sess=None,
         *args,
         **kwargs
     ):
-        self.model_path = 'model/' + model_name
+        # Assertions
+        assert model_path is not None, 'model path is not specified'
+        assert sess is not None and name is None, 'If pre-defiend session is used, must provide name of this network'
+        assert name == 'global', 'Model name cannot be global'
+       
+        # Presets
+        if name is not None:
+            input_tensor = name + '/'  + input_tensor
+            output_tensor = name + '/'  + output_tensor
 
-        # Initialize Session and TF graph
-        self._initialize_network()
+        # Find checkpoint
+        ckpt = tf.train.get_checkpoint_state(model_path)
+        if not ckpt or not tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+            raise NameError('Error : Graph failed to load')
+        else:
+            print('Checkpoint Found.')
+            print('Call TF pretrained model:')
+            print('    checkpoint_path : {}'.format(ckpt.model_checkpoint_path))
+            print('    input_name : {}'.format(input_name))
+            print('    output_name : {}'.format(output_name))
 
-    def get_action(self, input_tensor):
+        # Set TF graph and session
+        if sess is None:
+            config = tf.ConfigProto(device_count = {'GPU': 0})  # Only use CPU
+            self.graph = tf.Graph()
+            self.sess = tf.Session(config=config, graph=self.graph)
+        else:  # Use given graph
+            self.graph = sess.graph
+
+        # Load and restore TF model
+        with self.graph.as_default():
+            self.saver = tf.train.import_meta_graph(
+                    meta_graph_or_file=ckpt.model_checkpoint_path+'.meta',
+                    clear_devices=True,
+                    import_scope=name,
+                )
+            iuv(self.sess)  # Initialize all undefined weights
+        self.state, self.action = self.reset_network_weight()
+        print('    TF policy loaded. {}'.format(name) )
+
+    def run_network(self, input_tensor):
         with self.sess.as_default(), self.sess.graph.as_default():
             feed_dict = {self.state: input_tensor}
             action_prob = self.sess.run(self.action, feed_dict)
 
-        action_out = [np.random.choice(self.action_space, p=prob / sum(prob)) for prob in action_prob]
-
-        return action_out
+        return action_prob
 
     def reset_network_weight(self):
-        ckpt = tf.train.get_checkpoint_state(self.model_path)
-        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
-            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
-        else:
-            raise AssertionError
-
-    def _initialize_network(self, verbose=False):
-        """reset_network
-        Initialize network and TF graph
         """
-        def vprint(*args):
-            if verbose:
-                print(args)
+        Reload the weight from the TF meta data
+        """
+        input_name = self.input_name
+        output_name = self.output_name
+        with self.sess.graph.as_default():
+            ckpt = tf.train.get_checkpoint_state(self.model_dir)
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+            state = self.graph.get_tensor_by_name(input_name)
+            try:
+                action = self.graph.get_operation_by_name(output_name)
+            except ValueError:
+                action = self.graph.get_tensor_by_name(output_name)
+        return state, action
 
-        input_tensor = self.input_tensor
-        output_tensor = self.output_tensor
-
-        config = tf.ConfigProto(device_count={'GPU': 0})
-
-        # Reset the weight to the newest saved weight.
-        ckpt = tf.train.get_checkpoint_state(self.model_path)
-        vprint(f'path find: {ckpt.model_checkpoint_path}')
-        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
-            vprint(f'path exist : {ckpt.model_checkpoint_path}')
-            self.graph = tf.Graph()
-            with self.graph.as_default():
-                self.sess = tf.Session(config=config)
-                self.saver = tf.train.import_meta_graph(
-                    ckpt.model_checkpoint_path + '.meta',
-                    clear_devices=True
-                )
-                self.saver.restore(self.sess, ckpt.model_checkpoint_path)
-                vprint([n.name for n in self.graph.as_graph_def().node])
-
-                self.state = self.graph.get_tensor_by_name(input_tensor)
-
-                try:
-                    self.action = self.graph.get_operation_by_name(output_tensor)
-                except ValueError:
-                    self.action = self.graph.get_tensor_by_name(output_tensor)
-                    vprint([n.name for n in self.graph.as_graph_def().node])
-
-            vprint('Graph is succesfully loaded.', ckpt.model_checkpoint_path)
-        else:
-            vprint('Error : Graph is not loaded')
-            raise NameError
