e31ba4289918b019c3440d50f4dfc1e1eb4435e2
skim0119@gmail.com <skim0119@gmail.com>
2019-07-03 07:09:29 +0900

    update

--- policy/__init__.py
+++ policy/__init__.py
@@ -4,5 +4,3 @@ from policy.zeros import Zeros
 from policy.random import Random
 from policy.defense import Defense
 from policy.astar_flag import AStar
-from policy.policy_RL import PPO
-from policy.policy_PPO_meta import PPO_multimodes
