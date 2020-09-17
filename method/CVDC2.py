a8e306cb5a3b6bd7a86df0c654ecddd20efced90
skim0119 <skim0119@gmail.com>
2020-09-17 04:02:49 -0500

    update

---
+++ method/CVDC2.py
@@ -0,0 +1,247 @@
+import os
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+
+import numpy as np
+
+from utility.utils import store_args
+from utility.logger import *
+
+from network.CVDC_model2 import Central, Decentral
+from network.CVDC_model2 import get_gradient, train
+from network.CVDC_model2 import loss_central
+from network.CVDC_model2 import loss_ppo
+
+
+class SF_CVDC:
+    # Full architecture for centralized value training and
+    # decentralized control.
+    @store_args
+    def __init__(
+        self,
+        central_obs_shape,
+        decentral_obs_shape,
+        action_size,
+        agent_type,
+        save_path,
+        atoms=256,
+        lr=1e-4,
+        **kwargs
+    ):
+        assert type(agent_type) is list, "Wrong agent type. (e.g. 2 ground 1 air : [2,1])"
+        self.num_agent_type = len(agent_type)
+
+        # Set Model
+        self.dec_models = []
+        self.dec_optimizers = []
+        self.dec_checkpoints = []
+        self.dec_managers = []
+
+
+        # Build Network (Central)
+        self.model_central = Central(central_obs_shape[1:], atoms=atoms)
+        self.save_directory_central = os.path.join(save_path, 'central')
+        self.optimizer_central = tf.keras.optimizers.Adam(lr)
+        self.checkpoint_central = tf.train.Checkpoint(
+                optimizer=self.optimizer_central, model=self.model_central)
+        self.manager_central = tf.train.CheckpointManager(
+                checkpoint=self.checkpoint_central,
+                directory=self.save_directory_central,
+                max_to_keep=3,
+                keep_checkpoint_every_n_hours=1)
+
+        # Build Network (Decentral)
+        for i in range(self.num_agent_type):
+            model = Decentral(
+                    decentral_obs_shape[1:],
+                    action_size=5,
+                    atoms=atoms,
+                    prebuilt_layers=None if i == 0 else self.dec_models[0])
+            save_directory = os.path.join(save_path, 'decentral{}'.format(i))
+            optimizer = tf.keras.optimizers.Adam(lr)
+            checkpoint = tf.train.Checkpoint(
+                   optimizer=optimizer, model=model)
+            manager = tf.train.CheckpointManager(
+                    checkpoint=checkpoint,
+                    directory=save_directory,
+                    max_to_keep=3,
+                    keep_checkpoint_every_n_hours=1)
+            self.dec_models.append(model)
+            self.dec_optimizers.append(optimizer)
+            self.dec_checkpoints.append(checkpoint)
+            self.dec_managers.append(manager)
+
+        # PPO Configuration
+        self.ppo_config = {
+                'eps': tf.constant(0.20, dtype=tf.float32),
+                'entropy_beta': tf.constant(0.005, dtype=tf.float32),
+                'psi_beta': tf.constant(0.0, dtype=tf.float32),
+                'decoder_beta': tf.constant(1e-2, dtype=tf.float32),
+                'critic_beta': tf.constant(1, dtype=tf.float32),
+                'q_beta': tf.constant(1, dtype=tf.float32),
+                'learnability_beta': tf.constant(0.0, dtype=tf.float32),
+                }
+        # Critic Training Configuration
+        self.central_config = {'critic_beta': tf.constant(1.0)}
+
+    def log(self, step, log_weights=True, logs=None):
+        # Log specific part of the network
+        for i in range(self.num_agent_type):
+            model = self.dec_models[i]
+            sf_weight = model.sf_v_weight.get_weights()[0]
+            sf_q_weight = model.sf_q_weight.get_weights()[0]
+            tb_log_histogram(sf_weight, 'monitor/decentral{}_sf_v_weights'.format(i), step)
+            tb_log_histogram(sf_q_weight, 'monitor/decentral{}_sf_q_weights'.format(i), step)
+            tb_log_histogram(
+                    model.beta,
+                    'learnability_beta/decentral{}_beta'.format(i),
+                    step
+                )
+
+        # Log weights
+        if log_weights:
+            for var in self.model_central.weights:
+                tb_log_histogram(var.numpy(), 'central_weight/'+var.name, step)
+            for i in range(self.num_agent_type):
+                for var in self.dec_models[i].weights:
+                    tb_log_histogram(var.numpy(), 'decentral{}_weight/'.format(i)+var.name, step)
+
+        # Log Information
+        # - information must be given in dictionary form
+        if logs is not None:
+            for name, val in logs.items():
+                tf.summary.scalar(name, val, step=step)
+
+    def run_network_central(self, states):
+        # states: environment state
+        # observations: individual (centered) observation
+        env_critic, env_feature = self.model_central(states)
+        return env_critic, env_feature 
+
+    def run_network_decentral(self, states_list):
+        results = []
+        for states, model in zip(states_list, self.dec_models):
+            num_sample = states.shape[0]
+            temp_action = np.zeros(num_sample, dtype=np.int32)
+            actor, SF = model([states, temp_action])
+            actions = tf.random.categorical(actor["log_softmax"], 1, dtype=tf.int32).numpy().ravel()
+            results.append([actions, actor, SF])
+        return results
+
+    # Centralize updater
+    def update_central(self, datasets, writer=None, log=False, step=None, tag=None):
+        if log:
+            assert writer is not None
+            assert step is not None
+            assert tag is not None
+        critic_losses = []
+        
+        # Get gradients
+        model = self.model_central
+        optimizer = self.optimizer_central
+        for inputs in datasets:
+            _, info = train(model, loss_central, optimizer, inputs, self.central_config)
+            if log:
+                critic_losses.append(info["critic_mse"])
+        if log:
+            logs = {tag+'central_critic_loss': np.mean(critic_losses)}
+            with writer.as_default():
+                for name, val in logs.items():
+                    tf.summary.scalar(name, val, step=step)
+                writer.flush()
+    
+    # Decentralize updater
+    def update_decentral(self, datasets, writer=None, log=False, step=None, tag=None, image_log=False):
+        if log:
+            assert writer is not None
+            assert step is not None
+            assert tag is not None
+        actor_losses = []
+        dec_psi_losses = []
+        entropy = []
+        decoder_losses = []
+        critic_mse = []
+        q_losses = []
+        reward_mse = []
+        multiagent_value_loss = []
+        multiagent_reward_loss = []
+
+        learnability_loss = []
+
+        # Get gradients
+        for i in range(self.num_agent_type):
+            dataset = datasets[i]
+            model = self.dec_models[i]
+            optimizer = self.dec_optimizers[i]
+            for inputs in dataset:
+                _, info = train(model, loss_ppo, optimizer, inputs, self.ppo_config)
+                #grad, info = get_gradient(model, loss_ppo, inputs, self.ppo_config)
+                #optimizer.apply_gradients(zip(grad, model.trainable_variables))
+                if log:
+                    actor_losses.append(info['actor_loss'])
+                    dec_psi_losses.append(info['psi_loss'])
+                    entropy.append(info['entropy'])
+                    decoder_losses.append(info['generator_loss'])
+                    critic_mse.append(info['critic_mse'])
+                    q_losses.append(info['q_loss'])
+                    reward_mse.append(info['reward_loss'])
+                    learnability_loss.append(info['learnability_loss'])
+        if log:
+            logs = {tag+'dec_actor_loss': np.mean(actor_losses),
+                    tag+'dec_psi_loss': np.mean(dec_psi_losses),
+                    tag+'dec_entropy': np.mean(entropy),
+                    tag+'dec_generator_loss': np.mean(decoder_losses),
+                    tag+'dec_critic_loss': np.mean(critic_mse),
+                    tag+'dec_q_loss': np.mean(q_losses),
+                    tag+'dec_reward_loss': np.mean(reward_mse),
+                    tag+'dec_learnability_loss': np.mean(learnability_loss),
+                    }
+            with writer.as_default():
+                self.log(step, logs=logs)
+                writer.flush()
+
+        if image_log:
+            with writer.as_default():
+                for i in range(self.num_agent_type):
+                    test_dataset = datasets[i]
+                    for inputs in dataset:
+                        test_states = inputs['state']
+                        break
+                    n_state = test_states.shape[0]
+                    idx = np.random.randint(low=0, high=n_state, size=8)
+                    test_states = tf.gather(test_states, indices=idx, axis=0)
+                    _, SF = self.dec_models[i]([test_states, np.zeros(8, dtype=int)])
+                    tb_log_ctf_frame(test_states, tag=f'given states agent{i}', step=step)
+                    tb_log_ctf_frame(SF['decoded_state'], tag=f'decoded states agent{i}', step=step)
+                    tb_log_ctf_frame(SF['filtered_decoded_state'], tag=f'filtered states agent{i}', step=step)
+                writer.flush()
+
+    # Save and Load
+    def initiate(self, verbose=1):
+        cent_path = self.manager_central.restore_or_initialize()
+        if verbose:
+            print('Centralized initialization: {}'.format(cent_path))
+
+        for i in range(self.num_agent_type):
+            path = self.dec_managers[i].restore_or_initialize()
+            if verbose:
+                print('Decentralized{} initialization: {}'.format(i, path))
+
+        if cent_path == None:
+            return 0
+        else:
+            return int(cent_path.split('/')[-1].split('-')[-1])
+
+    def restore(self):
+        status = self.checkpoint_central.restore(self.manager_central.latest_checkpoint)
+        for i in range(self.num_agent_type):
+            checpoint = self.dec_checkpoints[i]
+            manager = self.dec_managers[i]
+            status = checkpoint[i].restore(manager.latest_checkpoint)
+
+    def save(self, checkpoint_number):
+        self.manager_central.save(checkpoint_number)
+        for i in range(self.num_agent_type):
+            manager = self.dec_managers[i]
+            manager.save(checkpoint_number)
+
---
+++ network/CVDC_model2.py
@@ -0,0 +1,311 @@
+import os
+import sys
+import math
+
+from functools import partial
+
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.layers as layers
+import tensorflow.keras.backend as K
+import tensorflow_probability as tfp
+
+from network.attention import Non_local_nn
+from network.model_V4_30 import V4, V4INV
+from network.model_V4_30 import V4Decentral, V4INVDecentral
+from utility.utils import store_args
+from utility.tf_utils import tf_clipped_log as tf_log
+
+import numpy as np
+
+
+class Decentral(tf.keras.Model):
+    @store_args
+    def __init__(self, input_shape, action_size=5, atoms=128,
+            prebuilt_layers=None, trainable=True):
+        super(Decentral, self).__init__()
+
+        if prebuilt_layers is None:
+            # Feature Encoding
+            self.feature_layer = V4(input_shape, action_size)
+            self.pi_layer = V4(input_shape, action_size)
+
+            # Decoder
+            self.action_dense1 = layers.Dense(units=128, activation='elu')
+            self.decoder_pre_dense1 = layers.Dense(units=128, activation='elu')
+            self.decoder_dense1 = layers.Dense(units=128, activation='elu')
+            self.decoder = V4INVDecentral()
+        else:
+            # Feature Encoding
+            self.feature_layer = prebuilt_layers.feature_layer
+            self.pi_layer = prebuilt_layers.pi_layer
+
+            # Decoder
+            self.action_dense1 = prebuilt_layers.action_dense1
+            self.decoder_pre_dense1 = prebuilt_layers.decoder_pre_dense1
+            self.decoder_dense1 = prebuilt_layers.decoder_dense1
+            self.decoder = prebuilt_layers.decoder
+
+        # Phi
+        self.phi_dense1 = layers.Dense(units=atoms, activation='relu')
+        self.sf_v_weight = layers.Dense(units=1, activation='linear', use_bias=False,)
+        self.sf_q_weight = layers.Dense(units=action_size, activation='linear', use_bias=False,)
+
+        # Actor
+        self.actor_dense1 = layers.Dense(128, activation='relu')
+        self.actor_dense2 = layers.Dense(action_size)
+        self.softmax = layers.Activation('softmax')
+        self.log_softmax = layers.Activation(tf.nn.log_softmax)
+
+        # Psi
+        self.psi_dense1 = layers.Dense(units=atoms, activation='relu')
+        self.psi_dense2 = layers.Dense(units=atoms, activation='relu')
+        self.smoothed_pseudo_H = tf.Variable(1.0)
+
+        # Learnabilty Maximizer
+        beta = np.ones([atoms,1], dtype=np.float32)
+        self.beta = tf.Variable(
+                initial_value=beta,
+                name='feature_scale',
+                dtype=tf.float32,
+                constraint=tf.keras.constraints.MinMaxNorm(rate=0.99, axis=1),
+            )
+
+        # Loss
+        self.mse_loss_mean = tf.keras.losses.MeanSquaredError()
+        self.mse_loss_sum = tf.keras.losses.MeanSquaredError(
+                reduction=tf.keras.losses.Reduction.SUM)
+
+    def print_summary(self):
+        self.feature_layer.summary()
+
+    def call(self, inputs):
+        # Run full network
+        obs = inputs[0]
+        action = inputs[1] # Action is included for decoder
+        action_one_hot = tf.one_hot(action, self.action_size, dtype=tf.float32)
+
+        # Feature Encoding SF-phi
+        net = self.feature_layer(obs)
+        phi = self.phi_dense1(net)
+        phi = phi / tf.norm(phi, ord=1, axis=1, keepdims=True)
+
+        # Actor
+        net = self.pi_layer(obs)
+        net = tf.concat([net, tf.stop_gradient(phi)], axis=1)
+        net = self.actor_dense1(net)
+        net = self.actor_dense2(net)
+        softmax_logits = self.softmax(net)
+        log_logits = self.log_softmax(net)
+
+        # Decoder
+        dec_net = self.decoder_pre_dense1(phi)
+        act_net = self.action_dense1(action_one_hot)
+        net = tf.math.multiply(dec_net, act_net)
+        net = self.decoder_dense1(net)
+        decoded_state = self.decoder(net)
+
+        psi = self.psi_dense1(tf.stop_gradient(phi))
+        psi = self.psi_dense2(psi)
+
+        net = self.psi_dense1(phi)
+        net = self.psi_dense2(net)
+        critic = self.sf_v_weight(net, training=True)
+        critic = tf.reshape(critic, [-1])
+        q = self.sf_q_weight(net, training=True)
+
+        beta = tf.math.abs(self.beta)
+        wv = self.sf_v_weight.weights[0]
+        wq = self.sf_q_weight.weights[0]
+        wv_neg = wv * (1.0-beta)
+        reward_predict = tf.linalg.matmul(phi, wv_neg)
+        reward_predict = tf.reshape(reward_predict, [-1])
+        inv_critic = tf.linalg.matmul(psi, wv_neg)
+        inv_critic = tf.reshape(inv_critic, [-1])
+
+        # For learnability update
+        wq_pos = tf.stop_gradient(wq) * beta
+        wv_neg = tf.stop_gradient(wv) * (1.0-beta)
+        psi_q_pos = tf.linalg.matmul(tf.stop_gradient(psi), wq_pos)
+        psi_v_neg = tf.linalg.matmul(tf.stop_gradient(psi), wv_neg)
+
+        # Filtered decoder
+        _phi = phi*tf.transpose(beta)
+        _dec_net = self.decoder_pre_dense1(_phi)
+        _net = tf.math.multiply(_dec_net, act_net)
+        _net = self.decoder_dense1(_net)
+        _decoded_state = self.decoder(_net)
+
+        actor = {'softmax': softmax_logits,
+                 'log_softmax': log_logits}
+        SF = {'reward_predict': reward_predict,
+              'phi': phi,
+              'psi': psi,
+              'critic': critic,
+              'decoded_state': decoded_state,
+              'Q': q,
+              'icritic': critic - inv_critic,
+              'psi_q_pos': psi_q_pos,
+              'psi_v_neg': psi_v_neg,
+              'filtered_decoded_state': _decoded_state
+              }
+
+        return actor, SF
+
+class Central(tf.keras.Model):
+    @store_args
+    def __init__(self, input_shape, atoms,
+                 trainable=True):
+        super(Central, self).__init__()
+
+        # Feature Encoding
+        self.feature_layer = V4(input_shape)
+
+        # Critic
+        self.critic_dense1 = layers.Dense(units=atoms, activation='relu')
+        self.successor_weight = layers.Dense(units=1, activation='linear', use_bias=False)
+
+        # Loss Operations
+        self.mse_loss_mean = tf.keras.losses.MeanSquaredError()
+        self.mse_loss_sum = tf.keras.losses.MeanSquaredError(
+                reduction=tf.keras.losses.Reduction.SUM)
+
+    def print_summary(self):
+        self.feature_layer.print_summary()
+
+    def call(self, inputs):
+        state = inputs
+
+        # Encoder
+        n_sample = state.shape[0]
+        net = self.feature_layer(state)
+
+        # Critic  
+        net = self.critic_dense1(net)
+        critic = self.successor_weight(net)
+
+        feature = {'latent': net}
+        SF = {'critic': critic}
+
+        return SF, feature 
+
+@tf.function
+def loss_central(model, state, td_target_c,
+        critic_beta):
+    inputs = state
+    SF, feature = model(inputs)
+
+    # Critic - TD Difference
+    critic_mse = model.mse_loss_mean(td_target_c, SF['critic'])
+
+    total_loss = critic_beta * critic_mse
+    info = {'critic_mse': critic_mse}
+
+    return total_loss, info
+
+@tf.function
+def loss_ppo(model, state, old_log_logit, action, old_value, td_target, advantage, td_target_c, rewards, next_state,
+        eps, entropy_beta, q_beta, psi_beta, decoder_beta, critic_beta, learnability_beta,):
+    num_sample = state.shape[0]
+
+    # Run Model
+    pi, v = model([state, action])
+    pi_next, v_next = model([next_state, action])
+    actor = pi['softmax']
+    psi = v['psi']
+    log_logits = pi['log_softmax']
+
+    # Reward Accuracy
+    reward_loss = model.mse_loss_sum(rewards, v['reward_predict'])
+    #reward_loss = 0.0
+
+    # Decoder loss
+    generator_loss = model.mse_loss_sum(state, v['decoded_state'])
+    #generator_loss = 0.0
+
+    # Entropy
+    H = -tf.reduce_mean(actor * tf_log(actor), axis=-1) # Entropy H of each sample
+    mean_entropy = tf.reduce_mean(H)
+    pseudo_H = tf.stop_gradient(
+            tf.reduce_sum(actor*(1-actor), axis=-1))
+    mean_pseudo_H = tf.reduce_mean(pseudo_H)
+    smoothed_pseudo_H = model.smoothed_pseudo_H
+
+    # Critic Loss
+    v_pred = v['critic']
+    v_pred_clipped = old_value + tf.clip_by_value(v_pred-old_value, -eps, eps)
+    critic_mse = tf.maximum(
+        tf.square(v_pred - td_target_c),
+        tf.square(v_pred_clipped - td_target_c))
+    critic_mse = tf.reduce_mean(critic_mse * tf.stop_gradient(smoothed_pseudo_H)) + tf.square(mean_pseudo_H-smoothed_pseudo_H)
+    
+    # Psi Loss
+    psi_mse = model.mse_loss_mean(td_target, psi)
+    #psi_mse = 0.0
+
+    # Actor Loss
+    action_one_hot = tf.one_hot(action, model.action_size, dtype=tf.float32)
+    log_prob = tf.reduce_sum(log_logits * action_one_hot, 1)
+    old_log_prob = tf.reduce_sum(old_log_logit * action_one_hot, 1)
+    ratio = tf.exp(log_prob - old_log_prob) # precision: log_prob / old_log_prob
+    surrogate = ratio * advantage # Clipped surrogate function
+    clipped_surrogate = tf.clip_by_value(ratio, 1-eps, 1+eps) * advantage
+    surrogate_loss = tf.minimum(surrogate, clipped_surrogate, name='surrogate_loss')
+    actor_loss = -tf.reduce_mean(surrogate_loss, name='actor_loss')
+
+    # Q - Loss
+    q = v['Q']
+    #q_a = tf.reduce_sum(q * tf.stop_gradient(actor), 1)
+    q_a = tf.reduce_sum(q * action_one_hot, 1)  # Current Q value
+    q_target = tf.stop_gradient(rewards + 0.98 * tf.reduce_max(v_next['Q'], axis=1))
+    #q_loss = tf.reduce_mean(tf.square(td_target_c - q_a))
+    q_loss = tf.reduce_mean(tf.square(q_target - q_a))
+
+    # L2 loss
+    #l2_loss = tf.nn.l2_loss(model.sf_v_weight.weights[0]) + tf.nn.l2_loss(model.sf_q_weight.weights[0])
+
+    # Learnability
+    var_action = tf.reduce_sum(tf.square(v['psi_q_pos']-v_next['psi_q_pos']) * tf.stop_gradient(pi['softmax']), axis=1)
+    var_environment = tf.square((v['psi_v_neg'] - v_next['psi_v_neg'])[:,0])
+    #learnability_loss = tf.reduce_mean(-var_action)
+    learnability_loss = tf.reduce_mean(-var_action+0.5*var_environment)
+
+    total_loss = actor_loss
+    total_loss += psi_beta*psi_mse
+    total_loss -= entropy_beta*mean_entropy
+    total_loss += decoder_beta*generator_loss
+    total_loss += critic_beta*critic_mse
+    total_loss += q_beta*q_loss
+    total_loss += learnability_beta*learnability_loss
+    #total_loss += 0.001*l2_loss
+
+    # Log
+    info = {'actor_loss': actor_loss,
+            'psi_loss': psi_mse,
+            'critic_mse': critic_mse,
+            'entropy': mean_entropy,
+            'generator_loss': generator_loss,
+            'q_loss': q_loss,
+            'reward_loss': reward_loss,
+            'learnability_loss': learnability_loss,
+            }
+
+    return total_loss, info
+
+def get_gradient(model, loss, inputs, hyperparameters={}):
+    with tf.GradientTape() as tape:
+        loss_val, info = loss(model, **inputs, **hyperparameters)
+    grads = tape.gradient(loss_val, model.trainable_variables,
+            unconnected_gradients=tf.UnconnectedGradients.ZERO)
+    return grads, info
+
+def train(model, loss, optimizer, inputs, hyperparameters={}):
+    with tf.GradientTape() as tape:
+        total_loss, info = loss(model, **inputs, **hyperparameters)
+    grads = tape.gradient(total_loss, model.trainable_variables)
+    optimizer.apply_gradients([
+        (grad, var)
+        for (grad,var) in zip(grads, model.trainable_variables)
+        if grad is not None])
+    return total_loss, info
+
