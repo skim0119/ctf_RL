e2b66e67e46234bccc14aaf9dc0ea0b34dcda8a7
skim0119 <skim0119@gmail.com>
2020-09-19 22:26:02 -0500

    update

--- push_TL.sh
+++ push_TL.sh
@@ -1,2 +1,2 @@
-python runner.py --train_number 3 --machine TL --nbg 4 --nba 1 --silence --device 0
-python runner.py --train_number 3 --machine TL --nbg 3 --nba 2 --silence --device 1
+python runner.py --train_number 4 --machine TL --nbg 4 --nba 1 --silence --device 0
+python runner.py --train_number 4 --machine TL --nbg 3 --nba 2 --silence --device 1
