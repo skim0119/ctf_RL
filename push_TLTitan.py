98c57d00e5d7ac5b503820c9a07212c4a72273d5
skim0119 <skim0119@gmail.com>
2020-10-08 04:48:52 -0500

    updateg

--- push_TLTitan.py
+++ push_TLTitan.py
@@ -1,3 +1,6 @@
-python runner.py --train_number 00 --map_size 30 --machine TLTi --nbg 2 --nba 1 --device 0
-python runner.py --train_number 00 --map_size 30 --machine TLTi --nbg 3 --nba 2 --device 1
+python runner.py --train_number 21 --map_size 30 --machine TLTi --nbg 3 --nba 0 --device 0 &
+python runner.py --train_number 21 --map_size 30 --machine TLTi --nbg 5 --nba 0 --device 0 &
+python runner.py --train_number 21 --map_size 30 --machine TLTi --nbg 7 --nba 0 --device 0 &
+python runner.py --train_number 21 --map_size 30 --machine TLTi --nbg 6 --nba 1 --device 1 &
+python runner.py --train_number 21 --map_size 30 --machine TLTi --nbg 5 --nba 2 --device 1 
 
