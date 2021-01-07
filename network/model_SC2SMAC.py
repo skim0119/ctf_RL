8757efab3797ca7a88297ded9c8e8d030a65dd30
skim0119 <skim0119@gmail.com>
2021-01-07 11:40:31 -0600

    update

--- network/model_SC2SMAC.py
+++ network/model_SC2SMAC.py
@@ -25,7 +25,8 @@ class CentralEnc(tf.keras.Model):
         self.static_network = keras.Sequential([
             layers.Input(shape=input_shape),
             layers.Dense(units=64, activation='elu'),
-            layers.GRU(64),
+            layers.Dense(units=64, activation='elu'),
+            #layers.GRU(64),
             layers.Dense(units=64, activation='elu'),
         ])
 
@@ -46,7 +47,8 @@ class DecentralEnc(tf.keras.Model):
         self.static_network = keras.Sequential([
             layers.Input(shape=input_shape),
             layers.Dense(units=64, activation='elu'),
-            layers.GRU(64),
+            layers.Dense(units=64, activation='elu'),
+            #layers.GRU(64),
             layers.Dense(units=64, activation='elu'),
         ])
         self.dense1 = layers.Dense(units=128, activation='elu')
